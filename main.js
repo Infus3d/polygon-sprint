7622edb4fe004c070ed49d7eb5edfd357254b17a
Omar Muhammetkulyyev <omar99@iastate.edu>
2022-03-31 05:12:04 -0500

    added the world background and map for level 1 (prob easy version)

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
 window.addEventListener("load", function(event){
     "use strict";
 
+    class StuffManager {
+        constructor(){
+            this.tileSheetImage = undefined;
+            this.tileSheet_tile_size = undefined;
+            this.tileSheet_columns = undefined;
+            this.tileSheet_spacing = 2;
+
+            this.backgroundImage = undefined;
+        }
+
+        requestImage(url, callbackFunction){
+            let img = new Image();
+            img.src = url;
+            img.addEventListener("load", function(event){
+                callbackFunction(img);
+            });
+        }
+    }
+
     let render = function(){
-        display.clearCanvas();
+        // display.clearCanvas();
+        display.drawBackground(stuffManager.backgroundImage);
+        display.drawMap(stuffManager.tileSheetImage, stuffManager.tileSheet_columns, stuffManager.tileSheet_tile_size, stuffManager.tileSheet_spacing,
+             game.world.map, game.world.columns, game.world.tile_set.tile_size);
         display.drawPlayer(game.world.player, game.world.player.color);
         display.render();
     };
@@ -18,13 +40,24 @@ window.addEventListener("load", function(event){
         controller.keyDownUp(event.type, event.keyCode);
     };
 
-    let controller  = new Controller();
-    let display     = new Display(document.getElementById("gameCanvas"));
-    let game        = new Game();
-    let runner      = new Runner(1000 / 60, update, render);
+    let stuffManager    = new StuffManager();
+    let controller      = new Controller();
+    let display         = new Display(document.getElementById("gameCanvas"));
+    let game            = new Game();
+    let runner          = new Runner(1000 / 60, update, render);
 
     display.resize(game.world.width, game.world.height);
-    runner.start();
+    stuffManager.requestImage("img/tiles_spritesheet.png", (image) => {
+        stuffManager.tileSheetImage = image;
+        stuffManager.tileSheet_tile_size = 70;
+        stuffManager.tileSheet_columns = 12;
+        stuffManager.tileSheet_spacing = 2;
+
+        stuffManager.requestImage("img/backgroundImage.png", (image) => {
+            stuffManager.backgroundImage = image;
+            runner.start();
+        });
+    });
     window.addEventListener("keydown", keyDownUp);
     window.addEventListener("keyup", keyDownUp);
 });
--- src/display.js
+++ src/display.js
@@ -3,10 +3,33 @@ class Display {
         this.buffer = document.createElement("canvas").getContext("2d");
         this.context = canvas.getContext("2d");
 
+        this.drawMap = function(image, image_columns, image_tile_size, image_tile_spacing, map, map_columns, map_tile_size){
+            for(let i = map.length-1; i >= 0; i--){
+                let value = map[i] - 1;
+                if(value == -1) continue;
+                
+                let src_x = (value % image_columns) * (image_tile_size + image_tile_spacing);
+                let src_y = Math.floor(value / image_columns) * (image_tile_size + image_tile_spacing);
+                
+                let dest_x = (i % map_columns) * map_tile_size;
+                let dest_y = Math.floor(i / map_columns) * map_tile_size;
+
+                this.buffer.drawImage(image, src_x, src_y, image_tile_size, image_tile_size, dest_x, dest_y, map_tile_size, map_tile_size);
+            }
+        };
+
         this.drawPlayer = function (rectangle, color) {
             this.buffer.fillStyle = color;
             this.buffer.fillRect(Math.round(rectangle.x), Math.round(rectangle.y), rectangle.width, rectangle.height);
         };
+
+        this.drawObject = function(image, src_x, src_y, src_w, src_h, dest_x, dest_y, dest_w, dest_h){
+            this.buffer.drawImage(image, src_x, src_y, src_w, src_h, dest_x, dest_y, dest_w, dest_h);
+        };
+
+        this.drawBackground = function(image){
+            this.buffer.drawImage(image, 0, 0, this.buffer.canvas.width, this.buffer.canvas.height);
+        }
     }
     render() {
         this.context.drawImage(this.buffer.canvas, 0, 0);
