3d7cf94bbe459c51bd838f0841a198ec11752dd4
mrmazur1 <michaelrmaz@gmail.com>
2022-04-28 23:27:09 -0500

    added more sound effects

--- main.js
+++ main.js
@@ -168,6 +168,9 @@ window.addEventListener("load", function(event){
             game.world.player.moveRight();
         }
         if (controller.up.active == true) {
+            let jump = new Audio("sounds/soundEffects/jump.mp3");
+            jump.load();
+            jump.play();
             game.world.player.jump();
             controller.up.active = false;
         }
